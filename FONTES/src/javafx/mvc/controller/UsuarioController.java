e1bb4aa1992d67d5c8f7b263a2a1145643fb2cc0
Erica <ericaborges97@hotmail.com>
2019-05-16 22:04:53 -0300

    View e controller Usuario

--- FONTES/src/javafx/mvc/controller/UsuarioController.java
+++ FONTES/src/javafx/mvc/controller/UsuarioController.java
@@ -6,16 +6,27 @@
 package javafx.mvc.controller;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Observable;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.mvc.dao.UsuarioDao;
+import javafx.mvc.model.UsuarioModel;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -90,27 +101,67 @@ public class UsuarioController implements Initializable {
     private TextField txtPesquisarUsuario;
 
     @FXML
-    void btPesquisarClickUsuario(ActionEvent event) {
-
+    void btPesquisarClickUsuario(ActionEvent event) throws Exception {
+        listarUsuario();
     }
 
     @FXML
-    void btInserirClickUsuario(ActionEvent event) {
-
+    void btInserirClickUsuario(ActionEvent event) throws Exception {
+        boolean okClicked = showDialog(um);
+        if (okClicked){
+            this.ud.salvar(um);
+            listarUsuario();
+        }
     }
     
+    private UsuarioModel um;
     @FXML
-    void btSalvarClickUsuario(ActionEvent event) {
+    void btSalvarClickUsuario(ActionEvent event) throws Exception{
+        this.um.setNomeFuncionario(txtCadNomeUsuario.getText());
+        this.um.setNomeUsuario(txtCadUsuario.getText());
+        this.um.setSenhaUsuario(txtCadSenhaUsuario.getText());
+        this.um.setStatus(comboCadUsuario.getSelectionModel().getSelectedItem().toString());
+       
+       //this.um.setCliente(checkPermitirCliente.isSelected());
+       //this.um.setUsuario(checkPermitirUsuario.isSelected());
+       //this.um.setProduto(checkPermitirProduto.isSelected());
+                
 
     }
 
     @FXML
-    void btAlterarClickUsuario(ActionEvent event) {
-
+    void btAlterarClickUsuario(ActionEvent event) throws Exception{
+        UsuarioModel um = (UsuarioModel) TableViewUsuario.getSelectionModel().getSelectedItem();
+        if (um != null) {
+            boolean okClicked = showDialog(um);
+            if (okClicked){
+                this.ud.salvar(um);
+                listarUsuario();
+            }
+        } else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Seleciona!!!");
+            alert.show();
+        }
     }
 
     @FXML
-    void btExcluirClickExcluir(ActionEvent event) {
+    void btExcluirClickExcluir(ActionEvent event)throws Exception {
+        UsuarioModel um = (UsuarioModel) TableViewUsuario.getSelectionModel().getSelectedItem();
+        if (um != null) {
+            Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+            confirm.setContentText("Deseja excluir?");
+            Optional<ButtonType> opcao = confirm.showAndWait();
+            if(opcao.get().getButtonData().equals(ButtonBar.ButtonData.OK_DONE)) {
+                this.ud.remove(um);
+                listarUsuario();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Seleciona!!!");
+            alert.show();
+        
+        }
         
     }
 
@@ -129,6 +180,40 @@ public class UsuarioController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+    
+    private UsuarioDao ud;
+    //private List<UsuarioModel> lista;
+    //private ObservableList<UsuarioModel> listaObserver;
+    
+    
+    private void listarUsuario() throws Exception{
+        
+        TableViewColumnID.setCellValueFactory(new PropertyValueFactory<>("idUsuario"));
+        TableViewColumnNome.setCellValueFactory(new PropertyValueFactory<>("nomeFuncionario"));
+        TableViewColumnNomeUsuario.setCellValueFactory(new PropertyValueFactory<>("nomeUsuario"));
+        TableViewColumnStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        
+        String criterio; 
+        
+        if (!txtPesquisarUsuario.getText().trim().isEmpty()) {
+            
+            String par = txtPesquisarUsuario.getText(); 
+            if (!par.matches("[^´~%';-_\\/\\*]+")) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setContentText("Caracteres inválidos!");
+                alert.show();
+                return;
+            }
+            
+            criterio = "where nomeUsuario like'%" + par + "%'";
+        }
+        //listaObserver = FXCollections.observableArrayList();
+        
+    }
+
+    private boolean showDialog(UsuarioModel um) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
