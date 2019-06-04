22b1573ff53bb720d4ee12d377726acacf56c9b9
alissonhs <alisson_hs@hotmail.com>
2019-06-04 19:38:35 -0300

    Adicionado Status na tabela Pedido

--- BANCO/UserSeeder.sql
+++ BANCO/UserSeeder.sql
@@ -1,4 +1,29 @@
--- popula o banco com 50 usuÃ¡rios diferentes
+-- Populaç?o do Banco de Dados
+
+
+-- Tabela de Cliente
+drop procedure if exists clientSeed;
+
+delimiter //
+
+create procedure clientSeed()
+	Begin		
+		declare j int default 1;
+		while (i <= 10) do
+			Insert into `cliente` (nomeCliente,cpf,rg,genero,celular,`status`)
+			Values (concat('Consumidor ',j), concat('0'), concat('0'), concat('M'),concat('0'), concat('A');
+			Set j = j+1;
+		end while;	
+	End;
+ // 
+Call clientSeed();
+//
+drop procedure if exists clientSeed; 
+
+
+
+-- Popula Tabela usuários com 50 usuários --
+
 drop procedure if exists userSeed;
 DELIMITER //  
 CREATE PROCEDURE userSeed()   
