e6b6798042ccfda5ff8c4224ce108ad0d56e083c
kenernot <lukas_jacoby@hotmail.com>
2019-06-13 21:50:33 -0300

    johaan

---
+++ FONTES/src/javafx/mvc/events/LoginEfetuado.java
@@ -0,0 +1,14 @@
+package javafx.mvc.events;
+
+/**
+ *
+ * @author lukas
+ */
+
+public class LoginEfetuado extends EventoOcorrido {
+    
+    public LoginEfetuado() {
+        setNome("JAVAFX.MVC.EVENTS.LOGINEFETUADO");
+    }
+    
+}
