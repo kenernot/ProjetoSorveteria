97b40e4cbe326c278cde8ecb6f55402e49079099
Thiagohcrestani <thiago_crestani@hotmail.com>
2019-06-27 22:08:45 -0300

    Caixa Topzera

--- FONTES/src/javafx/mvc/controller/AberturaCaixaController.java
+++ FONTES/src/javafx/mvc/controller/AberturaCaixaController.java
@@ -7,15 +7,24 @@ package javafx.mvc.controller;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.mvc.Main;
+import javafx.mvc.contracts.Ouvinte;
 import javafx.mvc.dao.CaixaDao;
+import javafx.mvc.events.EventoFecharTela;
+import javafx.mvc.events.EventoOcorrido;
+import javafx.mvc.events.EventoTrocarTela;
 import javafx.mvc.model.CaixaModel;
 import javafx.mvc.services.CaixaAberto;
 import javafx.mvc.services.Conexao;
 import javafx.mvc.services.UsuarioLogado;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -24,34 +33,83 @@ import javafx.scene.control.TextField;
  *
  * @author thiag
  */
-public class AberturaCaixaController implements Initializable {
+public class AberturaCaixaController implements Initializable, Ouvinte {
 
     /**
      * Initializes the controller class.
      */
     @FXML
     private TextField edt_valorInic;
-    
+
     @FXML
     private Button btn_salvar;
-    
-      @FXML
+
+    @FXML
     void btnSalvarClick(ActionEvent event) throws SQLException {
-        CaixaDao dao = new CaixaDao(Conexao.getInstance().getConn());
-        this.caixa.setValorAbertura(Double.valueOf(edt_valorInic.getText()));
-        CaixaModel cm = dao.salvar(this.caixa);
-          System.out.println(cm.getIdCaixa());
-        this.caixa.setIdCaixa(cm.getIdCaixa());
-        CaixaAberto.getInstance().setCaixa(this.caixa);
+        Alert mens = new Alert(Alert.AlertType.ERROR);
+        Alert mens1 = new Alert(Alert.AlertType.INFORMATION);
+
+        if (edt_valorInic.getText().isEmpty()) {
+            mens.setContentText("Valor Inicial não pode ser vazio!!");
+            mens.show();
+        } else if (Integer.parseInt(edt_valorInic.getText()) < 0) {
+            mens.setContentText("Valor Inicial não pode ser negativo!!");
+            mens.show();
+        } else {
+            CaixaDao dao = new CaixaDao(Conexao.getInstance().getConn());
+            this.caixa.setValorAbertura(Double.valueOf(edt_valorInic.getText()));
+            CaixaModel cm = dao.salvar(this.caixa);
+            System.out.println(cm.getIdCaixa());
+            this.caixa.setIdCaixa(cm.getIdCaixa());
+            CaixaAberto.getInstance().setCaixa(this.caixa);
+
+            mens1.setContentText("Salvo com Sucesso!!");
+            mens1.show();
+            
+            EventoOcorrido eventoAberto = new EventoOcorrido();
+            eventoAberto.setSource(this);
+            
+            eventoAberto.setNome("CAIXA_ABERTO");
+            Main.avisaOuvintes(eventoAberto);
+            
+            EventoOcorrido eventofechar = new EventoFecharTela("AberturaCaixa");
+            eventofechar.setSource(this);
+            Main.avisaOuvintes(eventofechar);
+            
+        }
     }
     private CaixaModel caixa;
 
-    
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      this.caixa = new CaixaModel();
-      this.caixa.setIdUsuario(UsuarioLogado.getInstance().getUser().getIdUsuario());
-    }    
-    
+        this.caixa = new CaixaModel();
+        this.caixa.setIdUsuario(UsuarioLogado.getInstance().getUser().getIdUsuario());
+        Main.subscribe(this);
+    }
+
+    @Override
+    public void avisandoAqui(EventoOcorrido evento) {
+        if (evento.getNome().equals("LOGIN_EFETUADO")) {
+            CaixaDao dao = new CaixaDao(Conexao.getInstance().getConn());
+            ArrayList<CaixaModel> lista = new ArrayList<>();
+            try {
+                lista = dao.buscar("idUsuario = " + UsuarioLogado.getInstance().getUser().getIdUsuario() + " and dataAbertura like concat('%',date_format(now(),'%Y-%m-%d'),'%') ORDER BY idCaixa DESC limit 1");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            if (lista.size() == 0) {
+                EventoOcorrido evento1 = new EventoTrocarTela("AberturaCaixa");
+                evento.setSource(this);
+                Main.avisaOuvintes(evento1);
+            } else if (lista.get(0).getDataFechamento() == null) {
+                System.out.println("Caixa já aberto!");
+                CaixaAberto.getInstance().setCaixa(lista.get(0));
+            } else {
+                System.out.println("Caixa fechado!");
+                CaixaAberto.getInstance().setCaixa(lista.get(0));
+            }
+
+        }
+    }
+
 }
--- FONTES/src/javafx/mvc/controller/FechamentoCaixaController.java
+++ FONTES/src/javafx/mvc/controller/FechamentoCaixaController.java
@@ -17,13 +17,18 @@ import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.mvc.Main;
+import javafx.mvc.contracts.Ouvinte;
 import javafx.mvc.dao.CaixaDao;
 import javafx.mvc.dao.ItemCaixaDao;
+import javafx.mvc.events.EventoFecharTela;
+import javafx.mvc.events.EventoOcorrido;
 import javafx.mvc.model.CaixaModel;
 import javafx.mvc.model.ItemCaixaModel;
 import javafx.mvc.services.CaixaAberto;
 import javafx.mvc.services.Conexao;
 import javafx.mvc.services.UsuarioLogado;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -32,7 +37,7 @@ import javafx.scene.control.TextField;
  *
  * @author thiag
  */
-public class FechamentoCaixaController implements Initializable {
+public class FechamentoCaixaController implements Initializable, Ouvinte {
 
     @FXML
     private Button btn_cancelar;
@@ -54,15 +59,27 @@ public class FechamentoCaixaController implements Initializable {
 
     @FXML
     void btnSalvarClick(ActionEvent event) throws SQLException {
+        Alert mens1 = new Alert(Alert.AlertType.INFORMATION);
         System.out.println(this.caixa.getIdCaixa());
         this.caixa.setValorFinal(Double.valueOf(edt_saldoFinal.getText()));
         this.caixa.setInformarSaldo(Double.valueOf(edt_informarSaldo.getText()));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date hoje = new Date();
+        String dHoje = sdf.format(hoje);
+        this.caixa.setDataFechamento(dHoje);
         this.cd.salvar(this.caixa);
+        mens1.setContentText("Caixa Fechado com Sucesso!!");
+        mens1.show();
+        EventoOcorrido evento = new EventoFecharTela("FechamentoCaixa");
+        evento.setSource(this);
+        Main.avisaOuvintes(evento);
     }
 
     @FXML
     void btnCancelarClick(ActionEvent event) {
-
+        EventoOcorrido evento = new EventoFecharTela("FechamentoCaixa");
+        evento.setSource(this);
+        Main.avisaOuvintes(evento);
     }
 
     private CaixaModel caixa;
@@ -70,23 +87,17 @@ public class FechamentoCaixaController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Main.subscribe(this);
         cd = new CaixaDao(Conexao.getInstance().getConn());
         try {
-            setSingletonCaixa();
+            if (CaixaAberto.getInstance().getCaixa() == null) {
+                setSingletonCaixa();
+            }
         } catch (SQLException ex) {
             System.out.println("Deu ruimzao");
         }
 
-        this.caixa = CaixaAberto.getInstance().getCaixa();
-        ItemCaixaDao icd = new ItemCaixaDao(Conexao.getInstance().getConn());
-        if (this.caixa != null) {
-            try {
-                icd.buscar("idCaixa=" + this.caixa.getIdCaixa());
-                List<ItemCaixaModel> resultadosDao = (List<ItemCaixaModel>) icd.buscar("idCaixa=" + this.caixa.getIdCaixa());
-            } catch (SQLException ex) {
-                Logger.getLogger(FechamentoCaixaController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        atualizaCampos();
     }
 
     private void setSingletonCaixa() throws SQLException {
@@ -95,10 +106,50 @@ public class FechamentoCaixaController implements Initializable {
         String dHoje = sdf.format(hoje);
 
         ArrayList<CaixaModel> arrayCaixa = new ArrayList<>();
-        arrayCaixa = cd.buscar("dataAbertura like '%" + dHoje + "%' AND idUsuario = " + UsuarioLogado.getInstance().getUser().getIdUsuario() + " limit 1 ");
+        arrayCaixa = cd.buscar("idUsuario = " + UsuarioLogado.getInstance().getUser().getIdUsuario() + " and dataAbertura like concat('%',date_format(now(),'%Y-%m-%d'),'%') ORDER BY idCaixa DESC limit 1");
         if (arrayCaixa.size() > 0) {
             CaixaAberto.getInstance().setCaixa(arrayCaixa.get(0));
             System.out.println(arrayCaixa.get(0).getIdCaixa());
+        }
+    }
+
+    @Override
+    public void avisandoAqui(EventoOcorrido evento) {
+        if (evento.getNome().equals("CAIXA_ABERTO") || evento.getNome().equals("ITEM_CAIXA_INSERIDO")) {
+            atualizaCampos();
+        }
+    }
+
+    private void atualizaCampos() {
+        this.caixa = CaixaAberto.getInstance().getCaixa();
+        ItemCaixaDao icd = new ItemCaixaDao(Conexao.getInstance().getConn());
+        if (this.caixa != null) {
+            System.out.println("Caixa nao esta vazio");
+            try {
+                icd.buscar("idCaixa=" + this.caixa.getIdCaixa());
+                List<ItemCaixaModel> resultadosDao = (List<ItemCaixaModel>) icd.buscar("idCaixa=" + this.caixa.getIdCaixa());
+
+                Double entradas = 0.0;
+                Double saidas = 0.0;
+                for (ItemCaixaModel item : resultadosDao) {
+                    if (item.getTipoMov().toUpperCase().equals("E")) {
+                        entradas += item.getValor();
+                    } else if (item.getTipoMov().toUpperCase().equals("S")) {
+                        saidas += item.getValor();
+                    }
+                }
+
+                Double saldoF = this.caixa.getValorAbertura() + entradas - saidas;
+                edt_entradas.setText(entradas.toString());
+                edt_saidas.setText(saidas.toString());
+                edt_saldoFinal.setText(saldoF.toString());
+                edt_entradas.setDisable(true);
+                edt_saidas.setDisable(true);
+                edt_saldoFinal.setDisable(true);
+
+            } catch (SQLException ex) {
+                Logger.getLogger(FechamentoCaixaController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
